aa1a3eb1a4cbeefd9b855d10eb3d44cf4ceee302
lcjay2212 <cjayuu2212@gmail.com>
2019-11-05 17:23:09 +0800

    test

---
+++ .idea/app.js
@@ -0,0 +1,12 @@
+// var person = {
+//   firstNanem: "Cjay",
+//   lastName: "Sotto",
+//   age: 28,
+// };
+
+// console.log(person);
+
+var printBacon = function() {
+  console.log("Bacon is healthy");
+};
+printBacon();
